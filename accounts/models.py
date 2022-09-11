6e27e2b9253cdc908a0d34d313bb2f473bd671d1
Francis Seneve <francisseneve@gmail.com>
2022-09-11 17:35:01 +0100

    Bug fix in custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -42,7 +42,7 @@ class User(AbstractBaseUser):
       first_name = models.CharField(max_length=50)
       last_name = models.CharField(max_length=50)
       username = models.CharField(max_length=50, unique=True)
-      email = models.EmailFied(max_length=100, unique=True) 
+      email = models.EmailField(max_length=100, unique=True) 
       phone_number = models.CharField(max_length=12, blank=True)
       role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
 
