b1a798a6136d9421adc370ea6c1df2e60734828d
Francis Seneve <francisseneve@gmail.com>
2022-09-16 15:47:49 +0100

    Adding templates partials

---
+++ accounts/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save 
+from django.dispatch import receiver
+from .models import User, UserProfile
+
+@receiver(post_save, sender=User)
+def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+    
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        try:
+            profile = UserProfile.objects.get(user=instance)
+            profile.save()
+        except:
+            #Create a user profile if not exist
+            UserProfile.objects.create(user=instance)
+
+
+# post_save.connect(post_save_create_profile_receiver, sender=User)
--- foodOnline/settings.py
+++ foodOnline/settings.py
@@ -129,6 +129,10 @@ STATICFILES_DIRS = [
     'foodOnline/static'
 ]
 
+#Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
