377b977c27181b2ea920a7d83b29c9f16f1dc2b1
Francis Seneve <francisseneve@gmail.com>
2022-09-16 17:45:05 +0100

    Adding a register form

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def registerUser(request):
-      return HttpResponse('User register form')
+      return render(request, 'accounts/registerUser.html')
