4a85817b4063b1051e63a1a9acbddfdd45b6ce01
Francis Seneve <francisseneve@gmail.com>
2022-09-18 17:49:46 +0100

    Register user functionalities and homepage setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
 
 # Create your views here.
 def registerUser(request):
-      return render(request, 'accounts/registerUser.html')
+      if request.method == 'POST':
+            form = UserForm(request.POST)
+            if form.is_valid():
+                  password = form.cleaned_data['password']
+                  user = form.save(commit=False)
+                  user.set_password(password)
+                  user.role = User.CUSTOMER
+                  user.save()
+                  messages.success(request, 'Your account has been created successfully!')
+                  return redirect('registerUser')
+      else:
+            form = UserForm()
+      context = {
+            'form': form
+      }
+      return render(request, 'accounts/registerUser.html', context)
