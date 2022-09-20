20af8f036ba0d3e112a00f7c296fdfe467e4dfa3
Francis Seneve <francisseneve@gmail.com>
2022-09-20 11:04:49 +0100

    Vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
-from .models import User
+from vendor.forms import VendorForm
+from .models import User, UserProfile
 from django.contrib import messages
 
 # Create your views here.
@@ -21,3 +22,37 @@ def registerUser(request):
             'form': form
       }
       return render(request, 'accounts/registerUser.html', context)
+
+
+def registerVendor(request):
+      if request.method == 'POST':
+            #Store the data and create the user
+            form = UserForm(request.POST)
+            v_form = VendorForm(request.POST, request.FILES)
+            if form.is_valid() and v_form.is_valid:
+                  first_name = form.cleaned_data['first_name']
+                  last_name = form.cleaned_data['last_name']
+                  username = form.cleaned_data['username']
+                  email = form.cleaned_data['email']
+                  password = form.cleaned_data['password']
+                  user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+                  user.role = User.VENDOR
+                  user.save()
+                  vendor = v_form.save(commit=False)
+                  vendor.user = user
+                  user_profile = UserProfile.objects.get(user=user)
+                  vendor.user_profile = user_profile
+                  vendor.save()
+                  messages.success(request, 'Your account has been successfully created! please wait for the approval')
+                  return redirect('registerVendor')
+            else:
+                  print(form.errors)
+      else:
+            form = UserForm()
+            v_form = VendorForm()
+
+      context = {
+            'form': form,
+            'v_form': v_form
+      }
+      return render(request, 'accounts/registerVendor.html', context)
--- foodOnline/settings.py
+++ foodOnline/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #Installed apps
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'vendor.apps.VendorConfig'
 ]
 
 MIDDLEWARE = [
