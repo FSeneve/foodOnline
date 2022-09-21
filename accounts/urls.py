1536ea7dd9dae1cf123beed3fc1bd0b560585a5c
Francis Seneve <francisseneve@gmail.com>
2022-09-21 09:27:06 +0100

    Customer and Vendor registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-      path('registerUser', views.registerUser, name="registerUser"),
-      path('registerVendor', views.registerVendor, name="registerVendor")
+      path('registerUser/', views.registerUser, name="registerUser"),
+      path('registerVendor/', views.registerVendor, name="registerVendor"),
+
+      path('login/', views.login, name='login'),
+      path('logout/', views.logout, name='logout'),
+      path('myAccount/', views.myAccount, name="myAccount"),
+      path('custDashboard/', views.custDashboard, name='custDashboard'),
+      path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,32 @@ from django.shortcuts import render, redirect
 from .forms import UserForm
 from vendor.forms import VendorForm
 from .models import User, UserProfile
-from django.contrib import messages
+from django.contrib import messages, auth
+from .utils import detectUser
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.exceptions import PermissionDenied
+
+
+#Restrict the vendor from accessing the customer page
+def check_role_vendor(user):
+      if user.role == 1:
+            return True
+      else:
+            raise PermissionDenied
+
+#Restrict the customer from accessing the vendor page
+def check_role_customer(user):
+      if user.role == 2:
+            return True
+      else:
+            raise PermissionDenied
 
 # Create your views here.
 def registerUser(request):
-      if request.method == 'POST':
+      if request.user.is_authenticated: 
+            messages.warning(request, 'You are already logged in!')
+            return redirect('myAccount')
+      elif request.method == 'POST':
             form = UserForm(request.POST)
             if form.is_valid():
                   password = form.cleaned_data['password']
@@ -25,7 +46,10 @@ def registerUser(request):
 
 
 def registerVendor(request):
-      if request.method == 'POST':
+      if request.user.is_authenticated: 
+            messages.warning(request, 'You are already logged in!')
+            return redirect('myAccount')
+      elif request.method == 'POST':
             #Store the data and create the user
             form = UserForm(request.POST)
             v_form = VendorForm(request.POST, request.FILES)
@@ -56,3 +80,43 @@ def registerVendor(request):
             'v_form': v_form
       }
       return render(request, 'accounts/registerVendor.html', context)
+
+
+def login(request):
+      if request.user.is_authenticated: 
+            messages.warning(request, 'You are already logged in!')
+            return redirect('myAccount')
+      elif request.method == 'POST':
+            email = request.POST['email']
+            password = request.POST['password']
+
+            user = auth.authenticate(email=email, password=password)
+            if user is not None:
+                  auth.login(request, user)
+                  messages.success(request, 'You are logged in!')
+                  return redirect('myAccount')
+            else:
+                  messages.error(request, 'Invalid login credentials')
+                  return redirect('login')
+      return render(request, 'accounts/login.html')
+
+def logout(request):
+      auth.logout(request)
+      messages.info(request, 'You are logged out!')
+      return redirect('login')
+
+@login_required(login_url='login')
+def myAccount(request):
+      user = request.user 
+      redirectUrl = detectUser(user)
+      return redirect(redirectUrl)
+
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def custDashboard(request):
+      return render(request, 'accounts/custDashboard.html')
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def vendorDashboard(request):
+      return render(request, 'accounts/vendorDashboard.html')
